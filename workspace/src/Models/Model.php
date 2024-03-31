10c30299b784250690eb6944194502fc0088fa0b
Nguyễn Mạnh Thắng <nguyenmanhthang2000.fb@gmail.com>
2024-03-31 17:06:02 +0700

    Init Model

--- workspace/src/Models/Model.php
+++ workspace/src/Models/Model.php
@@ -4,5 +4,6 @@ namespace Src\Models;
 
 abstract class Model
 {
-    //
+    public int|null $created_at;
+    public int|null $updated_at;
 }
---
+++ workspace/src/Models/User.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Src\Models;
+
+class User extends Model
+{
+    public string $id;
+    public string $username;
+    public string $password;
+    public string $name;
+}
