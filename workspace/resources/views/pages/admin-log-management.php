c330825e4923b526377e78917baef305ba08a5cd
Nguyễn Mạnh Thắng <nguyenmanhthang2000.fb@gmail.com>
2024-04-16 16:14:40 +0700

    Update CMS Title

--- workspace/resources/views/pages/admin-log-management.php
+++ workspace/resources/views/pages/admin-log-management.php
@@ -8,7 +8,10 @@
 <body>
     <div id="admin-layout">
         <?php partial("admin-sidebar.php", compact("features")) ?>
-        <div class="admin-wrapper"></div>
+        <div class="admin-wrapper">
+            <h1 class="cms-title"><?php echo $seo->title ?></h1>
+            <section class="cms-section"></section>
+        </div>
     </div>
 
     <?php partial("generic-scripts.php") ?>
